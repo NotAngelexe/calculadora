49cba2ccac8e8ff3bf868e7f0b446c5cae766c6b
soteliin <soteliin5@gmail.com>
2023-06-13 02:07:18 -0600

    formulario resistencias

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmisorComponent } from './emisor/emisor.component';
 import { MenuComponent } from './menu/menu.component';
+import { DivVoltComponent } from './div-volt/div-volt.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'menu', pathMatch: 'full' },
   { path: 'menu', component: MenuComponent },
   { path: 'emisor', component: EmisorComponent },
+  { path: 'divisor', component: DivVoltComponent },
   { path: '**', redirectTo: 'menu', pathMatch: 'full' },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PolFijaComponent } from './pol-fija/pol-fija.component';
 import { DivVoltComponent } from './div-volt/div-volt.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EmisorComponent } from './emisor/emisor.component';
 import { MenuComponent } from './menu/menu.component';
 
@@ -19,7 +19,7 @@ import { MenuComponent } from './menu/menu.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,FormsModule
+    AppRoutingModule, FormsModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
