17d593ad07054aec38939c319c98cae7057ed9fc
soteliin <soteliin5@gmail.com>
2023-06-13 01:25:18 -0600

    componente emisor y menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { PolFijaComponent } from './pol-fija/pol-fija.component';
 import { DivVoltComponent } from './div-volt/div-volt.component';
 import { FormsModule } from '@angular/forms';
+import { EmisorComponent } from './emisor/emisor.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PolFijaComponent,
-    DivVoltComponent
+    DivVoltComponent,
+    EmisorComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
