a05b7b9b9ffba867617a68ff2568ca911b687698
NotAngelExe <floresangel727@gmail.com>
2023-06-14 03:05:35 -0600

    La extension VS es lo peor que pudo hacer la vida

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { EmisorComponent } from './emisor/emisor.component';
 import { MenuComponent } from './menu/menu.component';
 import { DivVoltComponent } from './div-volt/div-volt.component';
+import { PolRetroComponent } from './pol-retro/pol-retro.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'menu', pathMatch: 'full' },
   { path: 'menu', component: MenuComponent },
   { path: 'emisor', component: EmisorComponent },
   { path: 'divisor', component: DivVoltComponent },
+  { path: 'retro', component: PolRetroComponent },
   { path: '**', redirectTo: 'menu', pathMatch: 'full' },
 ];
 
--- src/app/pol-retro/pol-retro.component.ts
+++ src/app/pol-retro/pol-retro.component.ts
@@ -6,5 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./pol-retro.component.css']
 })
 export class PolRetroComponent {
+  Rb: number=0;
+  Rc: number=0;
+  rbT: number=0;
+  rcT: number=0;
+  vcc: number=0;
 
+  IbQToma: number = 0;
+  IcQToma: number = 0;
+  IeQToma: number = 0;
+  ICSat: number = 0;
+  VCEQToma: number = 0;
+  VRCToma: number = 0;
+  VCToma: number = 0;
+  VEToma: number = 0;
+  VBToma: number = 0;
+
+
+  transistores: [
+    {
+      nombre: string,
+      beta: number,
+      maxVCE:number,
+      maxIc:number,
+      imagen: string
+    },
+    {
+      nombre: string,
+      beta: number,
+      maxVCE:number,
+      maxIc:number,
+      imagen: string
+    },
+  ] = [
+      {
+        nombre: "MPS2222A",
+        beta: 240,
+        maxVCE:40,
+        maxIc:0.6,
+        imagen: 'assets/mps2222a.jpg'
+      },
+      {
+        nombre: "TIP41C",
+        beta: 143,
+        maxVCE:40,
+        maxIc:6,
+        imagen: 'assets/TIP41C.jpg'
+      }
+    ]
 }
